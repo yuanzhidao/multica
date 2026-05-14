5da5ee2439b138b4c85999feab2b30d552bf4d5d
yuanzhidao <yuanzhidao@outlook.com>
2026-05-14 16:43:21 +0800

    chore: integrate combined pr branches

--- server/pkg/db/queries/issue.sql
+++ server/pkg/db/queries/issue.sql
@@ -69,15 +69,15 @@ INSERT INTO issue (
 ) RETURNING *;
 
 -- name: LockIssueDuplicateKey :exec
-SELECT pg_advisory_xact_lock(hashtextextended($1::text, 0));
+SELECT pg_advisory_xact_lock(hashtextextended(sqlc.arg('key')::text, 0));
 
 -- name: FindActiveDuplicateIssue :one
 SELECT * FROM issue
 WHERE workspace_id = $1
   AND status NOT IN ('done', 'cancelled')
-  AND project_id IS NOT DISTINCT FROM $2::uuid
-  AND parent_issue_id IS NOT DISTINCT FROM $3::uuid
-  AND lower(btrim(regexp_replace(title, '[[:space:]]+', ' ', 'g'))) = $4
+  AND project_id IS NOT DISTINCT FROM sqlc.arg('project_id')::uuid
+  AND parent_issue_id IS NOT DISTINCT FROM sqlc.arg('parent_issue_id')::uuid
+  AND lower(btrim(regexp_replace(title, '[[:space:]]+', ' ', 'g'))) = sqlc.arg('normalized_title')
 ORDER BY created_at ASC
 LIMIT 1;
 
